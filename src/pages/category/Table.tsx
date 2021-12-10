e8afb91a114d3538f7bd92fe87ae4b753d447e62
Allan Alves <allanalvesmail@gmail.com>
2021-12-10 06:56:15 -0300

    Implementando sistema de busca

--- src/pages/category/Table.tsx
+++ src/pages/category/Table.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import format from 'date-fns/format';
 import parseISO from 'date-fns/parseISO';
 import { BadgeNo, BadgeYes } from '../../components/Badge';
@@ -7,11 +7,15 @@ import { Category, ListResponse } from '../../util/model';
 import categoryHttp from '../../util/http/category-http';
 import DefaultTable, {
     makeActionStyles,
+    MuiDataTableRefComponent,
     TableColumn,
 } from '../../components/Table';
 import { IconButton, MuiThemeProvider } from '@material-ui/core';
 import EditIcon from '@material-ui/icons/Edit';
 import { Link } from 'react-router-dom';
+import { useSnackbar } from 'notistack';
+import { FilterResetButton } from '../../components/Table/FilterResetButton';
+import useFilter from '../../hooks/useFilter';
 
 const columnsDefinition: TableColumn[] = [
     {
@@ -77,34 +81,104 @@ const columnsDefinition: TableColumn[] = [
     },
 ];
 
-type Props = {};
-const Table = (props: Props) => {
+const debounceTime = 300;
+const debouncedSearchTime = 300;
+const rowsPerPage = 15;
+const rowsPerPageOptions = [15, 25, 50];
+
+const Table = () => {
+
+    const snackbar = useSnackbar();
+    const subscribed = useRef(true);
     const [data, setData] = useState<Category[]>([]);
     const [loading, setLoading] = useState<boolean>(false);
+    const tableRef = useRef() as React.MutableRefObject<MuiDataTableRefComponent>;
+
+    const {
+        columns,
+        filterManager,
+        cleanSearchText,
+        filterState,
+        debouncedFilterState,
+        totalRecords,
+        setTotalRecords,
+    } = useFilter({
+        columns: columnsDefinition,
+        debounceTime: debounceTime,
+        rowsPerPage,
+        rowsPerPageOptions,
+        tableRef
+    });
 
     useEffect(() => {
-        let isSubscribed = true;
-        (async () => {
-            const { data } = await categoryHttp.list<ListResponse<Category>>();
-            if (isSubscribed) {
-                setData(data.data);
-            }
-        })();
-
+        subscribed.current = true;
+        getData();
         return () => {
-            isSubscribed = false;
-        };
-    }, []);
+            subscribed.current = false;
+        }
+    }, [
+        cleanSearchText(debouncedFilterState.search),
+        debouncedFilterState.pagination.page,
+        debouncedFilterState.pagination.per_page,
+        debouncedFilterState.order
+    ]);
+
+    async function getData() {
+        try {
+            const { data } = await categoryHttp.list<ListResponse<Category>>({
+                queryParams: {
+                    search: cleanSearchText(debouncedFilterState.search),
+                    page: debouncedFilterState.pagination.page,
+                    per_page: debouncedFilterState.pagination.per_page,
+                    sort: debouncedFilterState.order.sort,
+                    dir: debouncedFilterState.order.dir,
+                }
+            });
+            if (subscribed.current) {
+                setData(data.data);
+                setTotalRecords(data.meta.total);
+            }
+        } catch (error) {
+            console.error(error);
+            if (categoryHttp.isCancelledRequest(error)) {
+                return;
+            }
+            snackbar.enqueueSnackbar(
+                'Não foi possível carregar as informações',
+                { variant: 'error', }
+            )
+        }
+    }
+
 
     return (
         <MuiThemeProvider theme={makeActionStyles(columnsDefinition.length - 1)}>
             <DefaultTable
                 title=''
-                columns={columnsDefinition}
+                columns={columns}
                 data={data}
                 loading={loading}
+                debouncedSearchTime={debouncedSearchTime}
+                ref={tableRef}
                 options={{
+                    serverSide: true,
                     responsive: 'scrollMaxHeight',
+                    searchText: filterState.search as any,
+                    page: filterState.pagination.page - 1,
+                    rowsPerPage: filterState.pagination.per_page,
+                    rowsPerPageOptions,
+                    count: totalRecords,
+                    customToolbar: () => (
+                        <FilterResetButton
+                            handleClick={() => filterManager.resetFilter()}
+                        />
+                    ),
+                    onSearchChange: (value) => filterManager.changeSearch(value),
+                    onChangePage: (page) => filterManager.changePage(page),
+                    onChangeRowsPerPage: (perPage) =>
+                        filterManager.changeRowsPerPage(perPage),
+                    onColumnSortChange: (changedColumn: string, direction: string) =>
+                        filterManager.changeColumnSort(changedColumn, direction),
                 }}
             />
         </MuiThemeProvider>
